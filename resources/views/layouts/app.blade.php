e5d5574609575778f2243e61ea1707064d7e0355
Wic Gnic <wicgnic@pop-os.localdomain>
2020-10-27 16:44:49 +0100

    Aggiunto la voce Archivio alla barra in alto

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
     <title>@yield('title')</title>
 
     <!-- Scripts -->
-    <!--<script src="{{ asset('js/app.js') }}" defer></script>-->
+<!--<script src="{{ asset('js/app.js') }}" defer></script>-->
     <link rel="stylesheet" href="//cdn.datatables.net/plug-ins/9dcbecd42ad/integration/bootstrap/3/dataTables.bootstrap.css">
 
     <script src="{{ asset('js/sorttable.js') }}"></script>
@@ -21,63 +21,67 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
     <!-- Styles -->
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-    <!--<link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">-->
+<!--<link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">-->
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-light">
-          <a class="navbar-brand" href="{{ route('tables') }}">{{-- {{ config('app.name') }} --}} <img src="{{URL::asset('/img/artco_logo_trasp_slim.png')}}" class="align-middle" alt="ArtCO" style="max-height: 19px; width:auto"> </a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+<div id="app">
+    <nav class="navbar navbar-expand-md navbar-light bg-light">
+        <a class="navbar-brand" href="{{ route('tables') }}">{{-- {{ config('app.name') }} --}} <img src="{{URL::asset('/img/artco_logo_trasp_slim.png')}}" class="align-middle" alt="ArtCO" style="max-height: 19px; width:auto"> </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
-          </button>
+        </button>
 
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto font-weight-bolder" style="font-family: Quicksand, sans-serif; font-size: 0.95rem; padding-left: 2rem">
-              <li class="nav-item px-3">
-                <a class="nav-link" href="{{ url('menu') }}"><i class="fas fa-chair"></i>&nbsp;&nbsp;FORNITURE</a>
-              </li>
-              <li class="nav-item px-3">
-                  <a class="nav-link" href="{{ url('clients') }}"><i class="fas fa-handshake"></i>&nbsp;&nbsp;CLIENTI</a>
-              </li>
+                <li class="nav-item px-3">
+                    <a class="nav-link" href="{{ url('menu') }}"><i class="fas fa-chair"></i>&nbsp;&nbsp;FORNITURE</a>
+                </li>
+                <li class="nav-item px-3">
+                    <a class="nav-link" href="{{ url('clients') }}"><i class="fas fa-handshake"></i>&nbsp;&nbsp;CLIENTI</a>
+                </li>
 
-              <li class="nav-item px-3">
-                  <a class="nav-link" href="{{ url('users') }}"><i class="fas fa-user"></i>&nbsp;&nbsp;UTENTI</a>
-              </li>
+                <li class="nav-item px-3">
+                    <a class="nav-link" href="{{ url('users') }}"><i class="fas fa-user"></i>&nbsp;&nbsp;UTENTI</a>
+                </li>
 
             </ul>
-            <ul class="navbar-nav">
-              @auth
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  {{ Auth::user()->username }} <span class="caret"></span>
-                </a>
-                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="{{ route('logout') }}"
-                       onclick="event.preventDefault();
+            <ul class="navbar-nav font-weight-bolder" style="font-family: Quicksand, sans-serif; font-size: 0.95rem; padding-left: 2rem">
+                @auth
+                    <li >
+                        <a class="nav-link" href="{{ url('archive') }}"><i class="fas fa-archive"></i>&nbsp;&nbsp;ARCHIVIO </a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            {{ Auth::user()->username }} <span class="caret"></span>
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                               onclick="event.preventDefault();
                                      document.getElementById('logout-form').submit();">
-                        {{ __('Logout') }}
-                    </a>
+                                {{ __('Logout') }}
+                            </a>
 
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                        @csrf
-                    </form>
-                </div>
-              </li>
-              @endauth
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                        </div>
+                    </li>
+
+                @endauth
             </ul>
-          </div>
-        </nav>
+        </div>
+    </nav>
 
-        <main class="py-4">
-            @yield('content')
-        </main>
-    </div>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.5.3/jspdf.debug.js" integrity="sha384-NaWTHo/8YCBYJ59830LTz/P4aQZK1sS0SneOgAvhsIl3zBu8r9RevNg5lHCHAuQ/" crossorigin="anonymous"></script>
-    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+    <main class="py-4">
+        @yield('content')
+    </main>
+</div>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.5.3/jspdf.debug.js" integrity="sha384-NaWTHo/8YCBYJ59830LTz/P4aQZK1sS0SneOgAvhsIl3zBu8r9RevNg5lHCHAuQ/" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+<script src="{{ asset('js/bootstrap.min.js') }}"></script>
 
-    @yield('scripts')
+@yield('scripts')
 </body>
 </html>
